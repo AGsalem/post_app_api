7b115d8ac60fce8f54f731cdd4927a6f3c0651f3
AGsalem <agsalem2025@outlook.com>
2026-06-18 16:51:57 +0200

    add auth  in post

--- src/controller/posts/see.js
+++ src/controller/posts/see.js
@@ -1,14 +1,24 @@
 import db from "../../plugin/db.js";
 const seeAllpost = async (req, res) => {
     try {
+        const retoken = res.cookies.token
+        const [seePostSign] = await db.query("SELECT * FROM posts ORDER BY RAND()")
+        const [seePostNotSign] =await db.query('SELECT post FROM posts ORDER BY RAND() ')
 
-        const [see] = await db.query("SELECT * FROM posts ORDER BY RAND()")
-        return res.status(201).json(see)
+
+        if (!retoken) {
+            return res.status(201).json(seePostSign)
+        }
+
+        else {
+            return res.status(201).json(seePostNotSign)
+
+        }
     } catch (err) {
         return res.status(500).json("Internal Server Error")
     }
 }
-export default seeAllpost 
+export default seeAllpost
 // ملحوظة الملف دة للادمن عشان يقدر يشوف اي بوست براحتة عادي
 // روح لملف searchPost.js
 // عشان تقدر تشوف البحث عن مستخدم
