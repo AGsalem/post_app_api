8b01cdcb66a61d94210eb56d2647e954c4c1f1d3
AGsalem <agsalem2025@outlook.com>
2026-06-23 22:04:42 +0300

    finsh to push in services

--- src/plugin/db.js
+++ src/plugin/db.js
@@ -1,10 +1,10 @@
 import "dotenv/config"
 import mysql2 from "mysql2/promise"
-const db = await mysql2.createConnection(process.env.DB)
-db.connect((err)=>{
-   if(err){
-      console.error(err)
-   }
-})
+const db = mysql2.createPool(process.env.DB)
+// db.connect((err)=>{
+//    if(err){
+//       console.error(err)
+//    }
+// })
 export default db
 // ملاحظة مهمة جدا الاتصال بقاعدة البيانات مش بيتعرف في دالة .use
